81a951923b6b2babf339f30eac5566f185a3d2dd
Your Name <adamduke35@gmail.com>
2025-06-24 03:21:06 +0800

    New users.dart model for Firebase + Debugging code

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -1,31 +1,50 @@
+// lib/models/users.dart
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class User {
-  int? id;
-  String name;
-  String email;
-  String phone;
-  String password;
+  final String id;
+  final String name;
+  final String email;
+  final String phone;
+  final String username;
+  // Add any other boolean fields you might have, e.g., final bool isActivated;
 
   User({
-    this.id,
+    required this.id,
     required this.name,
     required this.email,
     required this.phone,
-    required this.password,
+    required this.username,
+    // Add if applicable: this.isActivated = false,
   });
 
-  Map<String, dynamic> toMap() => {
-        'id': id,
-        'name': name,
-        'email': email,
-        'phone': phone,
-        'password': password,
-      };
+  factory User.fromFirestore(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    print('--- Debugging User.fromFirestore ---');
+    print('Document ID: ${doc.id}');
+    print('Raw Data: $data');
+    // If you have other boolean fields, print their types/values too
+    // print('Type of data[\'isActivated\']: ${data['isActivated']?.runtimeType}');
+    // print('Value of data[\'isActivated\']: ${data['isActivated']}');
 
-  factory User.fromMap(Map<String, dynamic> map) => User(
-        id: map['id'],
-        name: map['name'],
-        email: map['email'],
-        phone: map['phone'],
-        password: map['password'],
-      );
+    return User(
+      id: doc.id,
+      name: data['name'] ?? '',
+      email: data['email'] ?? '',
+      phone: data['phone'] ?? '',
+      username: data['username'] ?? '',
+      // If you had 'isActivated', handle it safely:
+      // isActivated: (data['isActivated'] is bool) ? data['isActivated'] : (data['isActivated'].toString().toLowerCase() == 'true'),
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'name': name,
+      'email': email,
+      'phone': phone,
+      'username': username,
+      // 'isActivated': isActivated,
+    };
+  }
 }
